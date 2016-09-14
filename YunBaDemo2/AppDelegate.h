3a33240d3a1b015ec5df34fa4b77f4f0f80f052a
caihongji123 <yunbapro@yunbaprodemacbook-pro.local>
2016-09-14 09:40:28 +0800

    Initial Commit

---
+++ YunBaDemo2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YunBaDemo2
+//
+//  Created by 云巴pro on 16/9/14.
+//  Copyright © 2016年 SHENZHEN WEIZHIYUN TECHNOLOGY CO.LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YunBaDemo2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  YunBaDemo2
+//
+//  Created by 云巴pro on 16/9/14.
+//  Copyright © 2016年 SHENZHEN WEIZHIYUN TECHNOLOGY CO.LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
